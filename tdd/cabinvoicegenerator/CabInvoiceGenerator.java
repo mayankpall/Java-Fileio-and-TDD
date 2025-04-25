80623b18aa18f1833838182b3abbba88f25adfd3
Mayank Pal <mayankpal9654@gmail.com>
2025-04-25 15:40:59 +0530

    Update CabInvoiceGenerator.java

--- tdd/cabinvoicegenerator/CabInvoiceGenerator.java
+++ tdd/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -9,19 +9,19 @@ public class CabInvoiceGenerator {
     private static final double MINIMUM_FARE = 5;
     private static final double PREMIUM_MINIMUM_FARE = 20;
 
-    // Method to calculate fare for normal ride
+
     public double calculateFare(double distance, double time) {
         double fare = (distance * NORMAL_RATE_PER_KM) + (time * NORMAL_RATE_PER_MIN);
         return fare < MINIMUM_FARE ? MINIMUM_FARE : fare;
     }
 
-    // Method to calculate fare for premium ride
+
     public double calculatePremiumFare(double distance, double time) {
         double fare = (distance * PREMIUM_RATE_PER_KM) + (time * PREMIUM_RATE_PER_MIN);
         return fare < PREMIUM_MINIMUM_FARE ? PREMIUM_MINIMUM_FARE : fare;
     }
 
-    // Method to calculate total fare for multiple rides
+
     public double calculateTotalFare(double[][] rides, boolean isPremium) {
         double totalFare = 0;
         for (double[] ride : rides) {
@@ -34,7 +34,7 @@ public class CabInvoiceGenerator {
         return totalFare;
     }
 
-    // Method to generate enhanced invoice
+
     public String generateInvoice(double[][] rides, boolean isPremium) {
         double totalFare = calculateTotalFare(rides, isPremium);
         int totalRides = rides.length;
@@ -49,21 +49,21 @@ public class CabInvoiceGenerator {
                 + "Average Fare Per Ride: Rs. " + averageFare + "\n";
     }
 
-    // Main method to run the program (for testing purposes)
+
     public static void main(String[] args) {
         CabInvoiceGenerator generator = new CabInvoiceGenerator();
 
-        // Sample rides
+
         double[][] rides = {
-                {2, 10},  // 2 km, 10 min
-                {5, 20},  // 5 km, 20 min
-                {1, 3}    // 1 km, 3 min
+                {2, 10},  
+                {5, 20},  
+                {1, 3}    
         };
 
-        // Generate Normal Ride Invoice
+
         System.out.println(generator.generateInvoice(rides, false));
 
-        // Generate Premium Ride Invoice
+
         System.out.println(generator.generateInvoice(rides, true));
     }
 }
